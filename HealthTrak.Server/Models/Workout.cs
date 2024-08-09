1e46ee2ac454e71c34b4a0781d3dfafd7f2526bb
Evan Holloway <eholloway00@gmail.com>
2024-08-09 08:36:14 -0700

    Adds models Starts ID implementation

---
+++ HealthTrak.Server/Models/Workout.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthTrak.Server.Models;
+
+public class Workout
+{
+    int length {  get; set; }
+    int distance { get; set; }
+    int maxHearRate { get; set; }
+    int minHeartRate { get; set; }
+    int AvgHeartRate { get; set; }
+}
+
