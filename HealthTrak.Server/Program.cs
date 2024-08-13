7993cad6aed822125b1a41b0ff54b508dea17122
Evan Holloway <eholloway00@gmail.com>
2024-08-13 08:05:55 -0700

    Adds login

--- HealthTrak.Server/Program.cs
+++ HealthTrak.Server/Program.cs
@@ -1,16 +1,21 @@
 using HealthTrak.Server.Context;
+using HealthTrak.Server.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<HealthTrakDbDontext>(
     options => options.UseInMemoryDatabase("HealthTrakDb")
     );
+
 builder.Services.AddAuthorization();
+
 builder.Services.AddIdentityApiEndpoints<IdentityUser>()
     .AddEntityFrameworkStores<HealthTrakDbDontext>();
 
@@ -31,6 +36,22 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapIdentityApi<IdentityUser>();
+
 app.MapControllers();
+
+app.MapGet("/food", (HttpContext httpContext) =>
+{
+    var meal = new Food
+    {
+        calories = 100,
+        protein = 2,
+        carbs = 4,
+        fats = 5
+    };
+    return meal;
+})
+.WithName("GetWeatherForecast")
+.RequireAuthorization();
 
 app.Run();
