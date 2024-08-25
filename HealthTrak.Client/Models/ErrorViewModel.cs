405ff7554c3aae12fdbd8f17b230566aa4853bd8
Evan.Laptop <eholloway00@gmail.com>
2024-08-25 05:40:31 -0700

    again

--- HealthTrak.Client/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HealthTrak.Client.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
